4368de1587381ea226690abf92730d91c8f24d07
minhphan46 <21522345@gm.uit.edu.vn>
2023-10-19 00:39:31 +0700

    first commit

---
+++ app/src/main/java/com/example/ar/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.ar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.core.view.isGone
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import io.github.sceneview.SceneView
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.node.ModelNode
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sceneView: ArSceneView
+    lateinit var placeButton: ExtendedFloatingActionButton
+    private lateinit var modelNode: ArModelNode
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sceneView = findViewById(R.id.sceneView)
+        placeButton = findViewById(R.id.btnPlace)
+
+        placeButton.setOnClickListener {
+            placeModel()
+        }
+
+        modelNode = ArModelNode().apply {
+            loadModelGlbAsync(
+                glbFileLocation = "models/sofa.glb"
+            )
+            {
+                sceneView.planeRenderer.isVisible = true
+            }
+            onAnchorChanged = {
+                placeButton.isGone
+            }
+        }
+        sceneView.addChild(modelNode)
+    }
+
+    private fun placeModel(){
+        Log.e("TEST", "placeModel: ")
+        modelNode.anchor()
+        sceneView.planeRenderer.isVisible = false
+    }
+}
