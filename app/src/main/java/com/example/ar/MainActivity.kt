de865b0133a32737265cc23caea9182de0bbfd50
minhphan46 <21522345@gm.uit.edu.vn>
2023-10-19 08:12:33 +0700

    finish ar

--- app/src/main/java/com/example/ar/MainActivity.kt
+++ app/src/main/java/com/example/ar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ar
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,41 +11,69 @@ import io.github.sceneview.SceneView
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.node.ModelNode
+import io.github.sceneview.node.VideoNode
+import com.google.ar.core.Config
+import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.math.Position
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sceneView: ArSceneView
     lateinit var placeButton: ExtendedFloatingActionButton
     private lateinit var modelNode: ArModelNode
-
+    private lateinit var videoNode: VideoNode
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sceneView = findViewById(R.id.sceneView)
+        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
+            this.lightEstimationMode = Config.LightEstimationMode.DISABLED
+        }
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.ad)
         placeButton = findViewById(R.id.btnPlace)
 
         placeButton.setOnClickListener {
             placeModel()
         }
 
-        modelNode = ArModelNode().apply {
+        videoNode = VideoNode(sceneView.engine, scaleToUnits = 0.7f, centerOrigin = Position(y=-4f), glbFileLocation = "models/plane.glb", player = mediaPlayer, onLoaded = {_,_ ->
+            mediaPlayer.start()
+        })
+
+        modelNode = ArModelNode(sceneView.engine, PlacementMode.INSTANT).apply {
             loadModelGlbAsync(
-                glbFileLocation = "models/sofa.glb"
+                glbFileLocation = "models/plane.glb",
+                scaleToUnits = 1f,
+                centerOrigin = Position(-0.5f)
+
             )
             {
                 sceneView.planeRenderer.isVisible = true
+                val materialInstance = it.materialInstances[0]
             }
             onAnchorChanged = {
-                placeButton.isGone
+                placeButton.isGone = it != null
             }
+
         }
         sceneView.addChild(modelNode)
+        modelNode.addChild(videoNode)
     }
 
     private fun placeModel(){
         Log.e("TEST", "placeModel: ")
         modelNode.anchor()
         sceneView.planeRenderer.isVisible = false
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.stop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 }
